61499d2e740bb94001daf93462f1c49375913cfa
DamasMahardi <damasmahardi234@gmail.com>
2021-01-29 20:27:49 +0700

    first

---
+++ src/main/java/id/belajar/Springweb/controller/WelcomeController.java
@@ -0,0 +1,34 @@
+package id.belajar.Springweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class WelcomeController {
+    private String message= " Hello from Spring web ";
+    private List<String>  tasks= Arrays.asList("a", "b","c","d","e","f","g");
+
+    @GetMapping("/")
+    public String main(Model model){
+        model.addAttribute("message",message);
+        model.addAttribute("tasks",tasks);
+
+        return "welcome";
+    }
+
+    @GetMapping("/hello")
+    public  String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "")
+            String name, Model model){
+        model.addAttribute("message",name);
+        return "welcome";
+
+    }
+}
